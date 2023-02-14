6c185dfba586ee247f5717e68aaf6439d5dc4357
Austin Davis <davis.austin1.1.c@gmail.com>
2023-02-14 15:57:35 -0500

    conditionals-into practice 1-4

---
+++ main.js
@@ -0,0 +1,63 @@
+// (example 1)
+/*
+var mathProblems = [1 + 1 > 5, 4 * 5 < 20, 6 - 2 >= 0, 1 + 1 === 2]
+
+for (var i = 0; i < mathProblems.length; i++ ){
+if(mathProblems[i] == true){
+    console.log("True fact!");
+  } else {
+    console.log("LIES!")
+  }
+}
+*/
+//--------------------------------------------------------
+
+//(Example 2)
+/*
+const likesDogs = true
+
+if (likesDogs === true){
+    console.log("You're a dog person!")
+}else{
+    console.log("You're a cat person!")
+}
+*/
+//-----------------------------------------------
+
+//(Example 3)
+/*
+const personMe = {
+        name: "Austin",
+        language: "German"
+}
+
+if(personMe.language === "English"){
+    console.log(`Hello, ${personMe.name}!`)
+} else if (personMe.language === "Spanish"){
+    console.log(`Hola, ${personMe.name}!`)
+} else if (personMe.language === "French"){
+    console.log(`Bonjour, ${personMe.name}!`)
+} else {
+    console.log("Language not found.")
+}
+*/
+//-----------------------------------------------
+
+//(example 4)
+/*
+let grade = 96;
+
+if (grade >= 0 && grade <= 69){
+    console.log("You got an F")
+}else if (grade >= 70 && grade <= 76){
+    console.log("You got a D")
+}else if (grade >= 77 && grade <= 84){
+    console.log("You got a C")
+}else if (grade >= 85 && grade <= 92){
+    console.log("You got a B")
+}else if (grade >= 93 && grade <= 100){
+    console.log("You got an A")
+} else{
+    console.log("Please call parents...")
+}
+*/
